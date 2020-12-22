44bccb6bf8a45686c89432f36276bf4ec3221a3f
John Foster <jfoster@esri.com>
2020-12-22 10:48:42 -0800

    Dependency update

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,18 +2,6 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'esri-map-view',
-  commonjs: {
-    namedExports: {
-      "node_modules/esri-loader/dist/umd/esri-loader.js": [
-        "getScript",
-        "isLoaded",
-        "loadModules",
-        "loadScript",
-        "loadCss",
-        "utils"
-      ]
-    }
-  },
   outputTargets: [
     {
       type: 'dist',
